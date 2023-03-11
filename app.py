6a9615a19b75cfbd324317093cfd71db0a32b99e
Dixing Xu <dixing.xu@hextrust.com>
2023-03-11 20:36:20 +0800

    update demo

--- app.py
+++ app.py
@@ -2,34 +2,31 @@ import os
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
-
+# from flask_cors import CORS
 app = Flask(__name__)
+# CORS(app)
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+history = []
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        p = request.form["prompt"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(p),
             temperature=0.6,
+            max_tokens=500
         )
+        history.append({"question": p, "response": response.choices[0].text})
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
-    return render_template("index.html", result=result)
+    return render_template("index.html", result=result, history=history)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+def generate_prompt(prompt):
+    cap_prompt = prompt.capitalize()
+    return f"你是一个人工智能客服：: {cap_prompt}\n "
